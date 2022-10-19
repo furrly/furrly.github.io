71bafb08ad9815793575557c3fc7d0a1f06addc8
C1rdec <cedric_lampron@hotmail.com>
2022-10-19 14:30:38 -0400

    Add Sylc link

--- src/views/About.js
+++ src/views/About.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 import GeneralContent from './GeneralContent';
 
 const About = () => {
+  const linkStyle = {
+    color: "#717D86"
+  };
   const { t } = useTranslation();
 
   return (
     <GeneralContent>
       <h1>{t('about.title')}</h1>
-      <p>{t('about.p1')}</p>
+      <p><Trans i18nKey="about.p1">...<Link style={linkStyle} to={{pathname: "https://sylc.dev"}} target="_blank">...</Link></Trans></p>
     </GeneralContent>
-  );
+  );  
 }
 
 export default About;
