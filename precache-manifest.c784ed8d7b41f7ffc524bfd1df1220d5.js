ecee564aa8de6eee0d1db55bc0a8fa4be6b3b8ae
C1rdec <cedric_lampron@hotmail.com>
2022-10-19 20:12:40 -0400

    Updates

---
+++ precache-manifest.c784ed8d7b41f7ffc524bfd1df1220d5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "474e7eeec88b8bae7c357a18d289b4a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be59017c0c0d47908ef4",
+    "url": "/static/css/main.e846ecac.chunk.css"
+  },
+  {
+    "revision": "91bc960a738cb5d4021a",
+    "url": "/static/js/2.f2f2d9d9.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.f2f2d9d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be59017c0c0d47908ef4",
+    "url": "/static/js/main.68e24a70.chunk.js"
+  },
+  {
+    "revision": "b34a2d831d8b42b7d2df",
+    "url": "/static/js/runtime-main.8f4a1c5c.js"
+  },
+  {
+    "revision": "2ed66cc960554dcef6bb274f2844c3bb",
+    "url": "/static/media/app_store_en_RGB_blk_092917.2ed66cc9.svg"
+  },
+  {
+    "revision": "3cc57c98b81aa427d99ae1874ac0e35a",
+    "url": "/static/media/app_store_fr_RGB_blk_092917.3cc57c98.svg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "3519d5118322d8bee83cd515c1208a27",
+    "url": "/static/media/furrly.3519d511.svg"
+  },
+  {
+    "revision": "2b2b7da40e9fe37746226683d444215a",
+    "url": "/static/media/getFetch.2b2b7da4.cjs"
+  },
+  {
+    "revision": "d6d4a3a1f4fe7b02f5f8bdba2103f99f",
+    "url": "/static/media/google-play-badge-en-081922.d6d4a3a1.svg"
+  },
+  {
+    "revision": "d5a017c7b98e03b3f22d6936ec006ef4",
+    "url": "/static/media/google-play-badge-fr-091222.d5a017c7.svg"
+  },
+  {
+    "revision": "8040006958dde96eb158bdc6036b5e29",
+    "url": "/static/media/illustration-section-01.80400069.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2852f6cfe5b21e5dab0da11a57b4438a",
+    "url": "/static/media/litter-manager.2852f6cf.png"
+  },
+  {
+    "revision": "4e9b8c4a0153a60e89aaef2660b669a6",
+    "url": "/static/media/profile.4e9b8c4a.png"
+  },
+  {
+    "revision": "d0738f30718dec00238fed721f57ca79",
+    "url": "/static/media/queue-manager.d0738f30.png"
+  },
+  {
+    "revision": "6d9ef69c0ec86af6fb94d24a7183e194",
+    "url": "/static/media/thumbnail.6d9ef69c.png"
+  }
+]);
